8d8dec1411d1aa4cb6c440475e68cda7d7e463f7
Kevin Hoang <find.kevinhoang@gmail.com>
2015-06-06 16:25:23 -0400

    Treehouse Dynamic Site

---
+++ app.js
@@ -0,0 +1,15 @@
+//Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
+//Solution: Use Node.js to perform the profile look ups and server our temple http
+
+var router = require("./router.js");
+
+//Create a web server
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+  router.home(request,response);
+  router.user(request,response);
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
